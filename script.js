645fbe0e5b6b8a9f7a91327140a40a43bc973a8f
sameh <sameh@localhost.localdomain>
2021-11-17 23:02:15 +0100

    complited

--- script.js
+++ script.js
@@ -5,12 +5,35 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass('sticky')
         }
+        if (this.scrollY > 500){
+            $('.scroll-up-btn').addClass('show')
+        } else {
+            $('.scroll-up-btn').removeClass('show')
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $("html").animate({scrollTop : 0})
     });
 
     $(".menu-btn").click(function(){
         $(".navbar .menu").toggleClass("active");
         $(".menu-btn i").toggleClass("active");
     });
+
+    var typed = new Typed(".typing",{
+        strings:["Back-End Web Developer","Software Engineering","Freelancer"],
+        typeSpeed : 100 ,
+        backSpeed : 60,
+        loop : true
+    })
+    var typed = new Typed(".typing2",{
+        strings:["Back-End Web Developer","Software Engineering","Freelancer"],
+        typeSpeed : 100 ,
+        backSpeed : 60,
+        loop : true
+    })
+
     $(".carousel").owlCarousel({
         margin: 20,
         loop: true,
